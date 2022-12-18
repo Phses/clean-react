94fc15675c54ec5ed91877c5191332d5a750e046
Phses <phsouzaesilva@gmail.com>
2022-12-18 07:16:02 -0300

    feat: ensure axiosHttpClient call axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -4,6 +4,6 @@ import axios from 'axios'
 export class AxiosHttpClient {
   async post (params: HttpPostParams<any>): Promise<void> {
     // axios.post e uma forma de garantir qual verbo esta sendo chamado
-    await axios.post(params.url)
+    await axios.post(params.url, params.body)
   }
 }

a488cd825c719f39075271e7023be658dcc73bf1
Phses <phsouzaesilva@gmail.com>
2022-12-27 20:29:49 -0300

    refactor: add componentes to login file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,30 +1,20 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import Spinner from '@/presentation/Components/spinner/spinner'
-import Header from '@/presentation/Components/header/header'
+import { Header, Input, Footer, FormStatus } from '@/presentation/Components'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
       <Header />
-      <div className={Styles.form}>
+      <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu email"/>
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-        <input type="password" name="password" placeholder="Digite sua senha"/>
-          <span className={Styles.status}>🔴</span>
-        </div>
+        <Input type="email" name="email" placeholder="Digite seu email" />
+        <Input type="password" name="password" placeholder="Digite sua senha"/>
         <button type="submit" className={Styles.submit}>Entrar</button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner}/>
-          <span className={Styles.error}>Erro</span>
-        </div>
-      </div>
-      <div className={Styles.footer} />
+        <FormStatus />
+      </form>
+      <Footer />
     </div>
   )
 }

223d4469b64c711a2adeb233e341252a1cba53dc
Phses <phsouzaesilva@gmail.com>
2022-12-27 20:27:17 -0300

    refactor: add using memo to header component

--- src/presentation/Components/header/header.tsx
+++ src/presentation/Components/header/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import Logo from '../logo/logo'
 import Styles from './header-styles.scss'
 
@@ -11,4 +11,4 @@ const Header: React.FC = () => {
   )
 }
 
-export default Header
+export default memo(Header)
